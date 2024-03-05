1279c483433d8df3c781e25b89894422af91d036
nygusiaty <skotorowicz_221i3@e.zseeim.edu.pl>
2024-03-05 09:26:47 +0000

    studentmachine send

---
+++ cpp/cwiczenie2/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+float kalkulatorPredkosci(float droga,float czas) {
+    //droga = droga * 1000;
+    //czas = czas * 60;
+    return droga/czas;
+}
+
+int main(){
+	float czas = 0;
+	float odleglosc =0;
+
+	cout<<"podaj odleglosc przejechana przez samochod w kilometrach"<<endl;
+	cin>>odleglosc;
+	cout<<"podaj czas przejazdu samochodu w minutach"<<endl;
+	cin>>czas;
+	
+	float wynik = kalkulatorPredkosci(odleglosc, czas);
+
+	cout<<wynik<<"KM/h"<<endl;
+	
+	return 0;
+}
