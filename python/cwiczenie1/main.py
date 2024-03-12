f59612068431cb1ca64d78ffedb1e281ce31aa7b
nygusiaty <skotorowicz_221i3@e.zseeim.edu.pl>
2024-03-12 09:30:37 +0000

    wyslanie pracy

---
+++ python/cwiczenie1/main.py
@@ -0,0 +1,14 @@
+
+co=int (input("Wellcum in calculator :D wybierz co chcesz zrobic:\n1 - mnozenie\n2 - dzielenie\n 3 - odejmowanie\n 4 - dodawanie\n 5 - potenga"))
+x=int (input("Podaj pierwszom liczbe"))
+y=int  (input("Podaj drugom liczbe"))
+if co == 1:
+    print (x*y)
+elif co == 2:
+    print (x/y)
+elif co == 3:
+    print (x-y)
+elif co == 4:
+    print (x+y)
+
+
